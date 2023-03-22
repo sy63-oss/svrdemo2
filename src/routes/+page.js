6f3cb11c717455357265a27062b25bb8708edc27
Tonton-Blax <redval@gmail.com>
2023-03-22 17:21:41 +0100

    test

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
 import { PUBLIC_URL } from '$env/static/public'
  
 
-/** @type {import('./$types').PageServerLoad} */
+/** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
     const req = await fetch(`${PUBLIC_URL}/items/articles`)
     const {data} = await req.json();
